c4a06be8ce59fc03c68a34a1b6095107f1bab42c
Prangya <meherprangya@gmail.com>
2025-01-28 20:04:02 +0530

    all changes and animation are added successfully

--- script.js
+++ script.js
@@ -1,13 +1,57 @@
-var swiper = new Swiper(".mySwiper", {
-    effect: "coverflow",
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: "auto",
-    coverflowEffect: {
-      rotate: 50,
-      stretch: 0,
-      depth: 100,
-      modifier: 1,
-      slideShadows: true,
-    },
-});
+var openMenu = document.querySelector(".open-bar"); 
+var closeMenu = document.querySelector(".close-bar");
+let gitHub=document.querySelector(".github");
+let linkedIn=document.querySelector(".linkedin");
+let contactButton=document.querySelector(".contact");
+let downloadButton=document.querySelector(".download");
+
+const toggleMenu = () => {
+  var tl1 = gsap.timeline();
+  tl1.to(".slider", { // Remove extra space in the selector
+    top: 0, 
+    duration: 1.3
+  });
+  
+  tl1.from(closeMenu,{
+    top:-10,
+    opacity:0,
+    duration:0.8
+  })
+  tl1.from(".slider  li",{
+    x:100,
+    opacity:0,
+    duration:0.7,
+    stagger:0.5
+  })
+  tl1.pause();
+  
+  openMenu.addEventListener("click", () => {
+    tl1.play();
+  });
+  closeMenu.addEventListener("click", () =>{
+    tl1.reverse();
+  })
+}
+
+const toggleGit = () => {
+  gitHub.addEventListener("click",()=>{
+
+  })
+
+  linkedIn.addEventListener("click",()=>{
+
+  })
+
+  contactButton.addEventListener("click",()=>{
+
+  })
+
+  downloadButton.addEventListener("click",()=>{
+    
+  })
+}
+
+
+
+toggleMenu();
+toggleGit();
