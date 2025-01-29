93ce47f31ab4286447baea585a85fb273d3f8040
Prangya <meherprangya@gmail.com>
2025-01-29 09:38:51 +0530

    animation updated

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let gitHub=document.querySelector(".github");
 let linkedIn=document.querySelector(".linkedin");
 let contactButton=document.querySelector(".contact");
 let downloadButton=document.querySelector(".download");
-
+// let blackBox=document.querySelector(".black_box");
 const toggleMenu = () => {
   var tl1 = gsap.timeline();
   tl1.to(".slider", { // Remove extra space in the selector
@@ -51,7 +51,35 @@ const toggleGit = () => {
   })
 }
 
+const blackBoxAnimation= () =>{
+  let tl2=gsap.timeline();
+  tl2.from(".black_box h1",{
+    x:200,
+    opacity:0,
+    delay:1,
+    duration:1.3,
+    stagger:0.7
+  })
+
+  tl2.to(".black_box h1",{
+    x:-100,
+    opacity:0,
+    duration:0.8,
+    stagger:0.2
+  })
+  tl2.to(".black_box",{
+    opacity:0,
+    duration:1.5
+  })
+  tl2.to(".black_box",{
+    display:"none"
+  })
+
+}
+
+
 
 
 toggleMenu();
 toggleGit();
+blackBoxAnimation();
